425797fc8709471e153845fc596c0640b62e82d9
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2018-09-09 22:21:25 -0400

    Ready for submission.

--- Week2_Assignment1/Main.cpp
+++ Week2_Assignment1/Main.cpp
@@ -1,3 +1,6 @@
+// Name: Lilit Khamalyan
+// Class: CISS 243
+// Date: 09/09/2018
 #include <iostream>
 #include "PersonData.h"
 #include "CustomerData.h"
@@ -8,20 +11,22 @@ int main()
 {
 	// Define two instances of the CustomerData class 
 	CustomerData first;
-	CustomerData second("David", "Smith", "218 Hanson Ct", "Moberly", "NC", "28323", "5738254884", 9, true);
+	CustomerData second("David", "Smith", "218 Hanson Ct", "Moberly", "NC", "28323", "5738256984", 9, true);
 	// Call displayCustomer function 
 	displayCustomer(second);
 	return 0;
 }
 void displayCustomer(CustomerData c)
 {
-	cout << c.getLastName() << endl;
-	cout << c.getFirstName() << endl;
-	cout << c.getAddress() << endl;
-	cout << c.getCity() << endl;
-	cout << c.getState() << endl;
-	cout << c.getZip() << endl;
-	cout << c.getPhoneNumber() << endl;
-	cout << c.getCustomerNumber() << endl;
-	cout << c.getMailingList() << endl;
+	cout << "  Customer's Informaion" << endl;
+	cout << "-------------------------" << endl;
+	cout << "Last Name: " << c.getLastName() << endl;
+	cout << "First Name: " << c.getFirstName() << endl;
+	cout << "Address: " << c.getAddress() << endl;
+	cout << "City: " << c.getCity() << endl;
+	cout << "State: " << c.getState() << endl;
+	cout << "Zip:" << c.getZip() << endl;
+	cout << "Phone Number: " << c.getPhoneNumber() << endl;
+	cout << "Customer Number: " << c.getCustomerNumber() << endl;
+	cout << "Miling List: " << c.getMailingList() << endl;
 }
